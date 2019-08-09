8e5dbc55088972cc16abcb02655d6524778ba67d
ngirimana <38111965+ngirimana@users.noreply.github.com>
2019-08-09 13:16:01 +0200

    feature(index-page): add index page (#1)
    - add index page
    
    [Finishes #167633556]

---
+++ UI/js/script.js
@@ -0,0 +1,13 @@
+const openNav = () => {
+  document.querySelector('#mySidenav').style.width = "250px";
+}
+
+const closeNav = () => {
+  document.querySelector('#mySidenav').style.width = "0";
+}
+
+
+
+
+
+/*modal */
