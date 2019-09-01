7f806cc85ed20281b0f0df87ce8b49b28e702f3e
ngirimana <chadrackngirimana@gmail.com>
2019-09-01 21:54:05 +0200

    bug(travis): add corect name of babel/plugin-proposal-class-properties
    - add real name of dependency
    [Finishes #168237916]

--- server/middleware/admin.js
+++ server/middleware/admin.js
@@ -1,25 +1,14 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import User from '../models/user_model';
 import status from '../helpers/StatusCode';
 
 dotenv.config();
 
 const admin = (req, res, next) => {
   const token = req.header('x-auth-token');
-  if (!token) { return res.status(status.UNAUTHORIZED).send({ status: status.UNAUTHORIZED, error: 'Access denied. No token provided.' }); }
-
   try {
     const decoded_jwt = jwt.verify(token, 'process.env.SECRETEKEY');
-    // Go ahead and grab user_id from JWT
-    // and find if that id exists in our users[](later on would be)\
-    // because we can not trust that user still exists
-    if (!User.isUserExist(decoded_jwt.id)) {
-      return res.status(status.NOT_FOUND).send({ status: status.NOT_FOUND, error: 'The User associated with this token doesn\'t exist.' });
-    }
 
-    // check again if user is admin to be allowed to perform
-    // [change user to admin, delete review deemed inapropriate]
     if (!decoded_jwt.is_admin) {
       return res.status(status.FORBIDDEN).send({ status: status.FORBIDDEN, error: 'You are not authorized to perform this action.' });
     }
