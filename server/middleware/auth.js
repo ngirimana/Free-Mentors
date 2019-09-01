b37bdb8b387642d706331e2984a41fdaaa49eaf6
NGIRIMANA Schadrack <38111965+ngirimana@users.noreply.github.com>
2019-09-01 22:01:46 +0200

    bug(travis): add corect name of babel/plugin-proposal-class-properties (#59)
    - add real name of dependency
    [Finishes #168237916]

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,13 +7,9 @@ dotenv.config();
 
 const auth = (req, res, next) => {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(status.UNAUTHORIZED).send({ status: status.UNAUTHORIZED, error: ' Access denied no token provided ' });
-
   try {
     const decode_jwt = jwt.verify(token, 'process.env.SECRETEKEY');
-    // Go ahead and grab user_id from JWT
-    // and find if that id exists in our users[](later on would be)
-    // because we can not trust that user still exists
+
     if (!User.isUserExist(decode_jwt.id)) {
       // eslint-disable-next-line max-len
       return res.status(status.NOT_FOUND).send({ status: status.NOT_FOUND, error: 'The User associated with this token doesn\'t exist.' });
--- server/middleware/mentor.js
+++ server/middleware/mentor.js
@@ -1,24 +1,12 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import User from '../models/user_model';
 import status from '../helpers/StatusCode';
 
 dotenv.config();
 const mentor = (req, res, next) => {
   const token = req.header('x-auth-token');
-  if (!token) { return res.status(status.UNAUTHORIZED).send({ status: status.UNAUTHORIZED, error: 'Access denied. No token provided.' }); }
-
   try {
     const decoded_jwt = jwt.verify(token, 'process.env.SECRETEKEY');
-    // Go ahead and grab user_id from JWT
-    // and find if that id exists in our users[](later on would be)\
-    // because we can not trust that user still exists
-    if (!User.isUserExist(decoded_jwt.id)) {
-      return res.status(status.NOT_FOUND).send({ tatus: status.NOT_FOUND, error: 'The User associated with this token doesn\'t exist.' });
-    }
-
-    // check again if user is mentor to be allowed to perform
-    // [change user to accept or reject mentorship session request]
     if (!decoded_jwt.is_mentor) {
       return res.status(status.FORBIDDEN).send({ status: status.FORBIDDEN, error: 'You are not authorized to perform this action.' });
     }
