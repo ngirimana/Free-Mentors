9d956ef6785cb64f40a3f8bc73eb435b3fdcfb90
NGIRIMANA Schadrack <38111965+ngirimana@users.noreply.github.com>
2019-09-03 06:27:21 +0200

    bug(gh-pages): remove non UI files to gh-pages (#64)
    - remove api (challenge 2) files from gh-pages
    [Finishes #168254808]

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,13 +1,9 @@
 const openNav = () => {
-  document.querySelector('#mySidenav').style.width = "250px";
-}
+  document.querySelector('#mySidenav').style.width = '250px';
+};
 
 const closeNav = () => {
-  document.querySelector('#mySidenav').style.width = "0";
-}
+  document.querySelector('#mySidenav').style.width = '0';
+};
 
-
-
-
-
-/*modal */
+/* modal */
