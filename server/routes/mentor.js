21aec9a9448487265f25e757314bc115eb790a36
NGIRIMANA Schadrack <38111965+ngirimana@users.noreply.github.com>
2019-09-10 20:26:01 +0200

    #168411694 specific mentor api  (#80)
    * env
    
    * feat(mentors): add get mentors api
    
    - add view mentors api
    
    [Finishes #168405440]
    
    * set is_admin to false
    
    * feat(mentors): add view mentors'api test
    - add test
    - remove if from auth
    
    [Finishes #168405440]
    
    * deleted .env
    
    * feat(specificMentor): user can view specific mentor api
    
    - add specific mentor api
    
    - [Finishes #168411694]
    
    * feat(specificMentor): create mentor_controller file
    
    - add mentor_controller
    
    [Finishes #168411694]
    
    * feat(specificMentor): add mentor id to response
    
    add mentor_id in response

--- server/routes/mentor.js
+++ server/routes/mentor.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import UserController from '../controllers/user_controller';
+import MentorController from '../controllers/mentor_controller';
 import auth from '../middleware/auth';
 
 
 const router = express.Router();
 
-const { getAllMentors } = UserController;
+const { getAllMentors, getSpecificMentor } = MentorController;
 
 router.get('/mentors', auth.auth, getAllMentors);
+router.get('/mentors/:id', auth.auth, getSpecificMentor);
 
 export default router;
