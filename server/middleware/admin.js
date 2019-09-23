88b66e306d76acd150b378d7204f7b06afe3effe
ngirimana <chadrackngirimana@gmail.com>
2019-09-23 11:26:00 +0200

    refactoring

--- server/middleware/admin.js
+++ server/middleware/admin.js
@@ -2,15 +2,14 @@
 import dotenv from 'dotenv';
 import status from '../helpers/StatusCode';
 import verifyToken from '../helpers/verfyToken';
+import response from '../helpers/response';
 
 dotenv.config();
 
 const admin = (req, res, err, next) => {
   const jwtAdmin = verifyToken;
   if (!jwtAdmin) {
-    return res.status(status.BAD_REQUEST).send(
-      { status: status.BAD_REQUEST, error: err.message },
-    );
+    return response.errorMessage(req, res, status.BAD_REQUEST, err.message);
   }
   if (!jwtAdmin.is_admin) {
     return res.status(status.FORBIDDEN).send({ status: status.FORBIDDEN, error: 'You are not authorized to perform this action.' });
--- server/middleware/mentor.js
+++ server/middleware/mentor.js
@@ -2,15 +2,14 @@
 import dotenv from 'dotenv';
 import status from '../helpers/StatusCode';
 import verifyToken from '../helpers/verfyToken';
+import response from '../helpers/response';
 
 dotenv.config();
 
 const mentor = (req, error, res, next) => {
   const decodedJwtMentor = verifyToken;
   if (!decodedJwtMentor) {
-    return res.status(status.BAD_REQUEST).send(
-      { status: status.BAD_REQUEST, error: error.message },
-    );
+    return response.errorMessage(req, res, status.BAD_REQUEST, err.message);
   }
   if (!decodedJwtMentor.is_mentor) {
     return res.status(status.FORBIDDEN).send({ status: status.FORBIDDEN, error: 'You are not authorized to perform this action.' });
