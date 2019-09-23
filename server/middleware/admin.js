977bf1f46b1c7559a613362d4dbd606d962888d1
ngirimana <chadrackngirimana@gmail.com>
2019-09-23 10:52:09 +0200

    remove name duplication

--- server/middleware/admin.js
+++ server/middleware/admin.js
@@ -5,15 +5,15 @@ import status from '../helpers/StatusCode';
 dotenv.config();
 
 const admin = (req, res, error, next) => {
-  const token = req.header('x-auth-token');
+  const userToken = req.header('x-auth-token');
 
-  const decodedJwtAdmin = jwt.verify(token, process.env.SECRETEKEY);
-  if (!decodedJwtAdmin) {
+  const jwtAdmin = jwt.verify(userToken, process.env.SECRETEKEY);
+  if (!jwtAdmin) {
     return res.status(status.BAD_REQUEST).send(
       { status: status.BAD_REQUEST, error: error.message },
     );
   }
-  if (!decodedJwtAdmin.is_admin) {
+  if (!jwtAdmin.is_admin) {
     return res.status(status.FORBIDDEN).send({ status: status.FORBIDDEN, error: 'You are not authorized to perform this action.' });
   }
   next();
