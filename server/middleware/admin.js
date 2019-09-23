eab6f43a56fcc5019c882751d70abfd0262f2da6
ngirimana <chadrackngirimana@gmail.com>
2019-09-23 10:47:18 +0200

    remove name duplication

--- server/middleware/admin.js
+++ server/middleware/admin.js
@@ -7,13 +7,13 @@ dotenv.config();
 const admin = (req, res, error, next) => {
   const token = req.header('x-auth-token');
 
-  const decoded_jwt = jwt.verify(token, process.env.SECRETEKEY);
-  if (!decoded_jwt) {
+  const decodedJwtAdmin = jwt.verify(token, process.env.SECRETEKEY);
+  if (!decodedJwtAdmin) {
     return res.status(status.BAD_REQUEST).send(
       { status: status.BAD_REQUEST, error: error.message },
     );
   }
-  if (!decoded_jwt.is_admin) {
+  if (!decodedJwtAdmin.is_admin) {
     return res.status(status.FORBIDDEN).send({ status: status.FORBIDDEN, error: 'You are not authorized to perform this action.' });
   }
   next();
--- server/middleware/mentor.js
+++ server/middleware/mentor.js
@@ -7,13 +7,13 @@ dotenv.config();
 const mentor = (req, error, res, next) => {
   const token = req.header('x-auth-token');
 
-  const decoded_jwt = jwt.verify(token, process.env.SECRETEKEY);
-  if (!decoded_jwt) {
+  const decodedJwtMentor = jwt.verify(token, process.env.SECRETEKEY);
+  if (!decodedJwtMentor) {
     return res.status(status.BAD_REQUEST).send(
       { status: status.BAD_REQUEST, error: error.message },
     );
   }
-  if (!decoded_jwt.is_mentor) {
+  if (!decodedJwtMentor.is_mentor) {
     return res.status(status.FORBIDDEN).send({ status: status.FORBIDDEN, error: 'You are not authorized to perform this action.' });
   }
   next();
